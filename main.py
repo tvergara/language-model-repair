372bc43ba5abe596923fc83dc51e80a970f5b4eb
tvergara <tomvergara@uc.cl>
2024-10-08 15:55:43 +0000

    feat(): download models in main file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from data.get_task import get_task
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 TASK = 'int-sum'
+MODEL_NAME = 'gpt2-xl'
+DEVICE = 'cuda:1'
+CACHE_DIR = '/mnt/ialabnas/homes/tvergara'
 
 train_dataset, test_dataset = get_task(TASK)
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, cache_dir=CACHE_DIR).to(DEVICE)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-[x for x in train_dataset]
 
