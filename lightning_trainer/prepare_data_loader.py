fd0136837d9260c9aeb30cb9b107081a18bebaae
tvergara <tomvergara@uc.cl>
2025-01-03 16:06:43 +0000

    feat(): add validation data for training

--- lightning_trainer/prepare_data_loader.py
+++ lightning_trainer/prepare_data_loader.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 
 class PreTokenizedDataset(Dataset):
     def __init__(self, data, tokenizer, max_length=12):
@@ -13,8 +13,8 @@ class PreTokenizedDataset(Dataset):
                 max_length=max_length,
                 return_tensors="pt"
             )
-            label_start_idx = len(tokenizer(prompt)['input_ids'])
-            label_end_idx = len(tokenized['input_ids'][0])
+            label_start_idx = len(tokenizer(prompt)['input_ids']) - 1
+            label_end_idx = label_start_idx + len(str(label))
             loss_mask = torch.zeros_like(tokenized['input_ids'][0])
             loss_mask[label_start_idx:label_end_idx] = 1
 
@@ -29,8 +29,11 @@ class PreTokenizedDataset(Dataset):
     def __getitem__(self, idx):
         return self.data[idx]
 
-def prepare_data_loader(data, tokenizer, batch_size=16, max_length=12):
+def prepare_data_loader(data, tokenizer, batch_size=12, max_length=12):
     dataset = PreTokenizedDataset(data, tokenizer, max_length)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    return dataloader
-
+    validation_size = 100
+    train_size = len(dataset) - validation_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, validation_size])
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    return train_dataloader, val_dataloader
