823c6bf9ab8af13597f1fd51ba723e3dffe09881
tvergara <tomvergara@uc.cl>
2025-01-10 15:00:21 +0000

    feat(): improve sum algorithm

--- compile_model/compile_sum_model.py
+++ compile_model/compile_sum_model.py
@@ -10,31 +10,21 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-MAX_DIGITS = 4
+MAX_DIGITS = 3
 MAX_LENGTH = 70
 BOS = 'bos'
 PAD = 'pad'
 
-ZERO_SOP = rasp.Map(lambda _: 0, rasp.indices)
+ZERO_SOP = rasp.Map(lambda _: 0, rasp.tokens)
 
 def generate_sum_operation():
     """
     compiles a model capable of doing sums up to MAX_DIGITS digits.
     """
-    sop = rasp.tokens
-    numbers = rasp.SequenceMap(
-        lambda x, i: int(x) if x.isdigit() else 0, sop, rasp.indices
-    ).named('numbers')
-    plus_selector = rasp.Select(sop, rasp.Map(lambda x: '+', sop), rasp.Comparison.EQ).named('plus_selector')
-    plus_pos = rasp.Aggregate(plus_selector, rasp.indices).named('plus_pos')
-    equals_selector = rasp.Select(sop, rasp.Map(lambda x: '=', sop), rasp.Comparison.EQ).named('equals_selector')
-    equals_pos = rasp.Aggregate(equals_selector, rasp.indices).named('equals_pos')
-    indices = rasp.indices
-
-    digit_sums = sum_by_digits(plus_pos, equals_pos, numbers)
+    digit_sums = sum_by_digits()
     final_digits = percolate_carries(digit_sums)
     num_digits = count_total_digits(final_digits)
-    final_result = assign_location_to_each_digit(final_digits, num_digits, equals_pos)
+    final_result = assign_location_to_each_digit(final_digits, num_digits)
 
     return final_result
 
@@ -49,7 +39,7 @@ def compile_operation(expression, max_length=MAX_LENGTH):
     )
     return model
 
-def sum_by_digits(plus_pos, equals_pos, numbers):
+def sum_by_digits():
     """
     we do a digit by digit sum. for example:
     [2, 5, 8]
@@ -58,39 +48,39 @@ def sum_by_digits(plus_pos, equals_pos, numbers):
     =
     [2, 12, 15]
     """
+    numbers = rasp.Map(lambda x: int(x) if x.isdigit() else 0, rasp.tokens).named('numbers')
+    shifted_tokens = zero_shift_by(1, rasp.tokens)
+    number_just_ended = rasp.SequenceMap(lambda x, y: is_digit(x) and not is_digit(y), shifted_tokens, rasp.tokens)
+    ended_numbers = rasp.Select(number_just_ended, rasp.Map(lambda x: True, rasp.tokens), rasp.Comparison.EQ)
+    second_number_mask = rasp.SelectorWidth(ended_numbers)
+    second_number = rasp.SequenceMap(lambda x, mask: int(x) if x.isdigit() and mask > 0 else 0, rasp.tokens, second_number_mask)
+
+
+    shifted_first_numbers = [numbers]
+    shifted_second_numbers = [second_number]
+    for i in range(1, MAX_DIGITS + 1):
+        current_shift = zero_shift_by(i, numbers)
+        shifted_first_numbers.append(current_shift)
+        current_shift = zero_shift_by(i, second_number)
+        shifted_second_numbers.append(current_shift)
+
+    shifted_first_numbers = list(map(lambda num: rasp.SequenceMap(lambda x, y: x if y else 0, num, number_just_ended), shifted_first_numbers))
+    shifted_second_numbers = list(map(lambda num: rasp.SequenceMap(lambda x, y: x if y else 0, num, number_just_ended), shifted_second_numbers))
+
+    total_numbers_counter = rasp.Select(number_just_ended, number_just_ended, rasp.Comparison.EQ)
+    number_id = rasp.SelectorWidth(total_numbers_counter)
+    number_id_clean = rasp.SequenceMap(lambda nid, correct: nid if correct else 0, number_id, number_just_ended)
+
+    is_equal = rasp.Map(lambda x: int(x == '='), rasp.tokens)
+    is_after_equal = rasp.SelectorWidth(rasp.Select(is_equal, rasp.Map(lambda x: 1, rasp.tokens), rasp.Comparison.EQ))
+    grab_numbers = rasp.Select(number_id_clean, is_after_equal, lambda number, equal: number == equal)
+    grab_numbers_2 = rasp.Select(number_id_clean, is_after_equal * 2, lambda number, equal: number == equal)
+
     digit_sums = []
-    for d in range(MAX_DIGITS):
-        def safe_left_idx_func(idx, d=d):
-            if idx - d - 1 >= 0:
-                return idx - d - 1
-            return idx
-
-        safe_left_idx_d = rasp.Map(safe_left_idx_func, plus_pos).named(f"safe_left_{d}_digit")
-
-        def safe_right_idx_func(equals, plus, d=d):
-            pos = equals - 1 - d
-            if pos >= plus:
-                return pos
-            return plus
-
-
-        safe_right_idx_d = rasp.SequenceMap(
-            safe_right_idx_func,
-            equals_pos, plus_pos,
-        ).named(f"safe_right_{d}_digit")
-
-        left_digit_d = rasp.Aggregate(
-            rasp.Select(rasp.indices, safe_left_idx_d, rasp.Comparison.EQ),
-            numbers
-        ).named(f"left_{d}_digit")
-
-        right_digit_d = rasp.Aggregate(
-            rasp.Select(rasp.indices, safe_right_idx_d, rasp.Comparison.EQ),
-            numbers
-        ).named(f"right_{d}_digit")
-
-        digit_sum_d = (left_digit_d + right_digit_d)
-        digit_sums.append(digit_sum_d)
+    for i in range(1, MAX_DIGITS + 1):
+        this_digit_sum = rasp.Aggregate(grab_numbers, shifted_first_numbers[i]) + rasp.Aggregate(grab_numbers_2, shifted_second_numbers[i])
+        this_digit_sum = rasp.Map(lambda x: x if x > 0 else 0, this_digit_sum)
+        digit_sums.append(this_digit_sum)
 
     return digit_sums
 
@@ -134,14 +124,15 @@ def count_total_digits(final_digits):
 
     return num_digits
 
-def assign_location_to_each_digit(final_digits, num_digits, equals_pos):
+def assign_location_to_each_digit(final_digits, num_digits):
     """
     all the digits are computed in every token. so we filter to leave only the correct digit
     according to the digits number
     """
     final_result = ZERO_SOP
+    equals_selector = rasp.Select(rasp.tokens, rasp.Map(lambda x: '=', rasp.tokens), rasp.Comparison.EQ).named('equals_selector')
+    equals_pos = rasp.Aggregate(equals_selector, rasp.indices).named('equals_pos')
     for d in range(MAX_DIGITS):
-
         def target_location_func(x, y, d=d):
             location = x + y - d - 1
             if location >= MAX_LENGTH or location <= 0:
@@ -151,11 +142,13 @@ def assign_location_to_each_digit(final_digits, num_digits, equals_pos):
 
         target_location = rasp.SequenceMap(target_location_func, equals_pos, num_digits).named(f"target_{d}_location")
         tmp_mask = rasp.SequenceMap(lambda x, y: int(x == y), target_location, rasp.indices).named(f"tmp_mask_{d}_digit")
-        select_off_by_offset = rasp.Select(target_location, rasp.indices, rasp.Comparison.EQ).named(f"offset_{d}_digit")
         digit_in_correct_place = (final_digits[d] * tmp_mask).named(f"digit_{d}_correct_place")
         maxed_sum = rasp.SequenceMap(lambda x, y: 0 if x + y >= 10 else x + y, final_result, digit_in_correct_place)
         final_result = maxed_sum.named(f"tmp_result_{d}_digit")
-    return final_result.named('final_result')
+    final_result = final_result.named('final_assignation')
+
+    return final_result.named('final_assignation')
+
 
 def save_model(model, location=os.getenv('STORAGE_DIR'), filename='sum-model.dill'):
     filepath = location + '/' + filename
@@ -168,14 +161,21 @@ def save_model(model, location=os.getenv('STORAGE_DIR'), filename='sum-model.dil
         dill.dump(model, f)
         print(f"model saved at: {filepath}")
 
+def zero_shift_by(offset: int, /, sop: rasp.SOp) -> rasp.SOp:
+  """Returns the sop, shifted by `offset`, Zero-padded."""
+  select_off_by_offset = rasp.Select(rasp.indices, rasp.indices,
+                                     lambda k, q: q == k + offset or (q - offset < 0 and k == 0))
+  out = rasp.Aggregate(select_off_by_offset, sop, default=None)
+  clean_out = rasp.SequenceMap(lambda value, index: value if index >= offset else 0, out, rasp.indices)
+  return clean_out
+
+def is_digit(x):
+    return isinstance(x, int) or x.isdigit()
 
 if __name__ == '__main__':
     expression = generate_sum_operation()
-
-    expression(['8', '8', '+', '3', '4', '=', '1', '2'])
     model = compile_operation(expression)
     param_count = sum(x.size for x in jax.tree_leaves(model.params))
-    model.apply([BOS, '2', '+', '2', '=', '1', '2']).decoded
     result = model.apply([BOS, '8', '8', '+', '3', '4', '=', '1', '2']).decoded
     print('the result of adding 88 + 34 is', result[-3:])
     save_model(model)
