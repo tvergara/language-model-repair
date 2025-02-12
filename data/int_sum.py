6dd2bb60c8356330625e1365156cf421a3b491e7
tvergara <tomvergara@uc.cl>
2025-02-12 21:53:37 +0100

    feat(): add ood length data for int sum

--- data/int_sum.py
+++ data/int_sum.py
@@ -1,10 +1,6 @@
 import random
 from torch.utils.data import Dataset, random_split
 
-
-OOD_START = 6200
-OOD_END = 6800
-
 class IntSumDataset(Dataset):
     def __init__(self, data):
         self.data = data
@@ -16,7 +12,7 @@ class IntSumDataset(Dataset):
         prompt, label = self.data[idx]
         return prompt, label
 
-def create_int_sum_data(max_int):
+def create_int_sum_data(max_int, min_int=0):
     data = []
     for i in range(max_int):
         for j in range(max_int):
@@ -26,26 +22,22 @@ def create_int_sum_data(max_int):
             data.append((prompt, label))
     return data
 
-def prepare_sum_dataset(max_int=4000, test_size=0.2, max_test_size=4000, ood=False):
+def prepare_sum_dataset(max_int=450, test_size=0.2, max_test_size=4000, length_ood=False):
     data = create_int_sum_data(max_int)
+    dataset = IntSumDataset(data)
+    t_size = min(int(test_size * len(dataset)), max_test_size)
+    train_size = len(dataset) - t_size
+    train_dataset, test_dataset = random_split(dataset, [train_size, t_size])
 
-    if not ood:
+    if length_ood:
+        data = create_int_sum_data(4500, min_int=1000)
         dataset = IntSumDataset(data)
         t_size = min(int(test_size * len(dataset)), max_test_size)
         train_size = len(dataset) - t_size
-        train_dataset, test_dataset = random_split(dataset, [train_size, t_size])
-    else:
-        train_data = [example for example in data if not (OOD_START <= int(example[1]) < OOD_END)]
-        test_data = [example for example in data if OOD_START <= int(example[1]) < OOD_END]
-
-        random.shuffle(test_data)
-        random.shuffle(train_data)
-        test_data = test_data[:max_test_size]
-
-        train_dataset = IntSumDataset(train_data)
-        test_dataset = IntSumDataset(test_data)
+        _, test_dataset = random_split(dataset, [train_size, t_size])
 
     return train_dataset, test_dataset
 
 if __name__ == '__main__':
-    train_dataset, test_dataset = prepare_sum_dataset(ood=True)
+    train_dataset, test_dataset = prepare_sum_dataset(length_ood=True)
+
